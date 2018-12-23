f14a447b6948f33e3cf69b0266077062da50770e
AlexandruAmarandei <aas1u16@soton.ac.uk>
2018-12-23 22:18:48 +0200

    Second update

--- src/App.js
+++ src/App.js
@@ -1,28 +1,53 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import SpotifyPlayer from 'react-spotify-player';
+import YouTube from 'react-youtube';
 import SpotifyWebApi from 'spotify-web-api-js';
+import { Button, ListGroup, ListGroupItem } from 'react-bootstrap';
+import SplitterLayout from 'react-splitter-layout';
+
+
+
 const spotifyApi = new SpotifyWebApi();
-// things to look into
-// should the queue be a state? i don't think so
-// normalize
+
+const size = {
+    width: '100%',
+    height: '390',
+};
+const view = 'list'; // or 'coverart'
+const theme = 'black'; // or 'white'
+const youtubeOptions = {
+    height: '390',
+    width: '100%',
+    playerVars: { // https://developers.google.com/youtube/player_parameters
+        autoplay: 1
+    }
+};
 class App extends Component {
+
+
     constructor(){
         super();
         const params = this.getHashParams();
-        const token = params.access_token;
+        var token = params.access_token;
         if (token) {
             spotifyApi.setAccessToken(token);
         }
         this.state = {
-            queue: [],
             loggedIn: token ? true : false,
+            spotifyURI : 'spotify:track:2SL6oP2YAEQbqsrkOzRGO4',
+            youtubeId: '2g811Eo7K8U',
+            queue: [],
+            votes: [],
             type: 's',
-            nowPlaying: { name: 'Not Checked', albumArt: '' }
+            nowPlaying: { name: 'Not Checked', albumArt: '' },
+            currentSong: 0,
+            token: token
         };
         // automatic refresh every 2 seconds
         let timerId = setInterval(() => this.updateQueueFromServer('tick'), 2000);
     }
+
 
     getHashParams() {
         var hashParams = {};
@@ -50,10 +75,13 @@ class App extends Component {
 
     // call this function at the end of a song
 
-    addSong( songLink){
+    addSong( songLink, votes){
       var newQueue = this.state.queue;
+      var newVotesQueue = this.state.votes;
       newQueue.push(songLink);
+      newVotesQueue.push(votes);
       this.setState({queue : newQueue});
+      this.setState({votes: newVotesQueue});
 
     }
 
@@ -62,6 +90,8 @@ class App extends Component {
     }
 
     updateQueueFromServer(){
+        this.addSong("spotify:track:2SL6oP2YAEQbqsrkOzRGO4", 0);
+        this.addSong("2g811Eo7K8U", 0);
         //var newQueue = getQueueFromServer();
         //this.setState({queue: newQueue});
     }
@@ -69,47 +99,134 @@ class App extends Component {
     // call this on the page refresh
     // we might need to expand this
     refreshPage(){
-        this.playNext();
+        //this.playNext();
     }
 
     playNext(){
       this.updateQueueFromServer();
+
         if (this.state.queue === undefined || this.state.queue.length == 0) {
             // if queue is empty
-            //
+            //             //
         }else {
-            var newCurrentSong = this.state.queue.shift();
-            //check if song is from youtube or from spotify
-            this.setState({type: 's'});
-            this.setState({nowPlaying: {name: newCurrentSong}});
+            this.playSong(0);
         }
     }
 
-    // to add spotify and youtube specific api
-  // and to add display a list of songs also giving the possiblity to play them now or remove them
+    playSong(songPosition){
+        var newCurrentSongs = this.state.currentSong + 1;
+        this.setState({currentSong: newCurrentSongs });
+        this.setState({type: "n"});
+        var newCurrentSong = this.state.queue[songPosition];
+        this.removeSong(songPosition);
+        if(newCurrentSong.startsWith('spotify:')) {
+            this.setState({type: 's', spotifyURI: newCurrentSong});
+        }else{
+            this.setState({type: 'y', youtubeId: newCurrentSong});
+        }
+
+
+    }
+
+    removeSong(songPosition){
+        this.state.queue.splice(songPosition, 1);
+        this.state.votes.splice(songPosition, 1);
+        // this should call our api and remove a song
+    }
     render() {
         return (
-            <div className="App">
+            <div >
+
                 <a href='http://localhost:8888' > Login to Spotify </a>
+                <a> Token: {this.state.token} </a>
                 <div>
                     Now Playing: { this.state.nowPlaying.name }
                 </div>
+                <button onClick={() => this.getNowPlaying("spotify:track:2SL6oP2YAEQbqsrkOzRGO4")}>
+                    Get now playing.
+                </button>
+                <SplitterLayout>
                 <div>
-                    <img src={this.state.nowPlaying.albumArt} style={{ height: 150 }}/>
-                </div>
-                { this.state.loggedIn &&
-                <button onClick={() => this.getNowPlaying()}>
-                    Check Now Playing
-                </button>
+                { this.state.type === "s" && this.state.currentSong % 2 === 1 &&
+                    <SpotifyPlayer
+                        uri= {this.state.spotifyURI}
+                        size={size}
+                        view={view}
+                        theme={theme}
+                    />
                 }
-                <button onClick={() => this.addSong()}>
-                    {this.state.queue}
-                </button>
+                { this.state.type === "s" && this.state.currentSong %2 === 0 &&
+                    <SpotifyPlayer
+                        uri= {this.state.spotifyURI}
+                        size={size}
+                        view={view}
+                        theme={theme}
+                    />
+                 }
+                {this.state.type === "y" && this.state.currentSong %2 === 1 &&
+                    <YouTube
+                        videoId= {this.state.youtubeId}
+                        opts={youtubeOptions}
+                    />
 
+                }
+                {this.state.type === "y" && this.state.currentSong %2 === 0 &&
+                    <YouTube
+                        videoId= {this.state.youtubeId}
+                        opts={youtubeOptions}
+                    />
+
+                }
+                </div>
+
+                <div>
+                    {this.displaySongs()}
+                </div>
+                </SplitterLayout>
 
             </div>
         );
     }
+
+    displaySongs(){
+        return(
+        <div className = "songs">
+        <ListGroup>
+            {this.displaySongList()}
+        </ListGroup>
+        </div>
+        );
+     }
+
+    displaySongList(){
+       var currentSongsInQueue = this.state.queue;
+       return currentSongsInQueue.map(
+            (song, i) =>
+                <ListGroupItem>
+                    { !song.startsWith('spotify:') &&
+                        <img src={require('./youtubeLogo.png')} width="50" height="50"/>
+                    }
+                    {  song.startsWith('spotify:') &&
+                        <img src={require('./spotifyLogo.png')} width="50" height="50"/>
+                    }
+                    <Button bsStyle="primary" onClick={() => this.playSong(i)}>
+                        Play
+                    </Button>
+                    {song}
+                    <span> </span> Votes: {this.state.votes[i]}
+                    <Button bsStyle="primary" onClick={() => this.removeSong(i)}>
+                        Remove
+                    </Button>
+                </ListGroupItem>
+       );}
+
+
+    _onReady(event) {
+        // access to player in all event handlers via event.target
+        event.target.pauseVideo();
+    }
+
+
 }
 
 export default App;
